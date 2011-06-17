87c98d7ee71ae1f1a3bf81b6b550303adee254e1
Masahiro Sakai <masahiro.sakai@gmail.com>
2011-06-17 17:55:39 +0900

    add state dump.
    Ignore-this: d7c48af40ecd92dfe1eacce5840b3dec
    
    darcs-hash:20110617085539-fad48-82a702442f33d37b339af5cb69c2d686482616e8.gz

--- Play.hs
+++ Play.hs
@@ -2,6 +2,8 @@ module Player where
 
 import Control.Monad
 import Control.Monad.State
+import qualified Data.IntMap as IM
+import Data.IntMap ((!))
 import LTG
 
 data LR = L | R deriving (Ord, Eq, Show, Enum, Bounded)
@@ -28,14 +30,24 @@ act (flag,c,i) = do
 play :: Player -> Opponent -> StateT GameState IO ()
 play = p1
   where
+    g (f,v) = [slot | i <- [0..255], let slot = (i, (v ! i, f ! i))
+                    , f ! i /= PAp I [] || v ! i /= 10000]
+    printState (p1,p2) = do
+      print $ g p1
+      print $ g p2
+
     p1 :: Player -> Opponent -> StateT GameState IO ()
     p1 p (Opponent op) = do
+      lift $ putStrLn "========= player 1"
+      get >>= \s -> lift (printState s)
       act (fst p)
       (a,b) <- get
       p2 (op (b,a)) (snd p)
 
     p2 :: Player -> Opponent -> StateT GameState IO ()
     p2 p (Opponent op) = do
+      lift $ putStrLn "========= player 2"
+      get >>= \s -> lift (printState s)
       act (fst p)
       s <- get
       p1 (op s) (snd p)
