4204ebfd1aee6dea33c07c2ab724a26016f3c0b6
shelarcy <shelarcy@gmail.com>
2011-06-17 20:59:13 +0900

    Add 'session2' (Another example session)
    Ignore-this: a1caf3b67e601d36a972a73f18e09a6a
    
    darcs-hash:20110617115913-ce356-081392e198eb7d2145a4a4948bdc0ebd3e1cad36.gz

--- Play.hs
+++ Play.hs
@@ -108,3 +108,30 @@ player1 = replay'
 session1 = runStateT (play player0 player1) initialState
 
 -- ---------------------------------------------------------------------------
+
+player2 = replay
+  [ (R, Help, 0)
+  , (R, Zero, 0)
+  , (L, K,    0)
+  , (L, S,    0)
+  , (R, Succ, 0)
+  , (R, Zero, 0)
+  , (R, Zero, 1)
+  , (L, Succ, 1)
+  , (L, Dbl,  1)
+  , (L, Dbl,  1)
+  , (L, Dbl,  1)
+  , (L, Dbl,  1)
+  , (L, K,    0)
+  , (L, S,    0)
+  , (R, Get,  0)
+  , (L, K,    0)
+  , (L, S,    0)
+  , (R, Succ, 0)
+  , (R, Zero, 0)
+  ]
+
+session2 = runStateT (only player2) initialState
+
+-- ---------------------------------------------------------------------------
+
