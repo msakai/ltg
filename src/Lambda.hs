7327399a39fae1ab6f65059bce970ddc23a02c71
Masahiro Sakai <masahiro.sakai@gmail.com>
2011-06-18 18:09:06 +0900

    modified Lambda.hs
    Ignore-this: 9c5b7e2ea0ae071cb7d8a8e3be08abb8
    
    darcs-hash:20110618090906-fad48-1a6a41fc10f6d78d5b638613cd57e1ff90929813.gz

--- src/Lambda.hs
+++ src/Lambda.hs
@@ -1,45 +1,37 @@
-module Lambda where
+module Lambda
+  ( Var
+  , Term (..)
+  , compile
+  ) where
 
 import LTG
 
+type Var = String
+
 data Term
-    = Var Int
+    = Var String
+    | Int !Int
     | Card Card
     | App Term Term
-    | Lambda Int Term
+    | Lambda Var Term
     deriving Show
 
-removeVar :: Int -> Term -> Term
-removeVar x (Var y) | x==y = (Card I)
-removeVar x (App f y) = App (App (Card S) (removeVar x f)) (removeVar x y)
+{-
+TODO: Term‚Å‚Í‚È‚­LTG‚ÌValue‚Ö‚ÆƒRƒ“ƒpƒCƒ‹‚µ‚½‚¢
+-}
+compile :: Term -> Term
+compile (Int i)      = Int i
+compile (Card c)     = Card c
+compile (App a b)    = app (compile a) (compile b)
+compile (Lambda v a) = removeVar v a
+
+removeVar :: Var -> Term -> Term
+removeVar x (Var y) | x==y = Card I
+removeVar x (Int i) = Int i
+removeVar x (App f y) = app (app (Card S) (removeVar x f)) (removeVar x y)
 removeVar x (Lambda y z) = removeVar x (removeVar y z)
-removeVar x y = App (Card K) y
+removeVar x y = app (Card K) y
 
-{-
-ski' :: Term -> Term
-ski' (App x y)    = App (ski' x) (ski' y)
-ski' (Lambda x (Var y))
-  | x == y    = I
-  | otherwise = App K (Var y)
-ski' (Lambda x (App y z)) = App (App S (ski' (Lambda x y))) (ski' (Lambda x z))
-ski' (Lambda x (Lambda y z)) = ski' (Lambda x (ski' (Lambda y z)))
-ski' (Lambda x y) = App K (ski' y)
-ski' x = x
--}
-
-f :: Term -> String
-f (App x y@(App _ _)) = f x ++ "(" ++ f y ++ ")"
-f (App x y) = f x ++ f y
-f (Card c) = show c
-
-{-
-f (removeVar 0 (Lambda 0 (Lambda 1 (Lambda 2 (App (Var 0) (App (Var 1) (Var 2)))))))
--}
-
-i x = x
-k x y = x
-s x y z = (x z) (y z)
-
-kiss = k i s s
-
-
+app :: Term -> Term -> Term
+app (Card K) (Card I) = Card Put
+app x y = App x y
