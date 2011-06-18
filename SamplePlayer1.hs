65973dd349d752c304c4265c0c82fabc7f985e00
Masahiro Sakai <masahiro.sakai@gmail.com>
2011-06-18 09:19:21 +0900

    add SamplePlayer1.hs
    Ignore-this: f2595b97c033a46540166616dcb076ea
    
    darcs-hash:20110618001921-fad48-cd9956129671ed683e57ed5411ec1423c04ef64b.gz

---
+++ SamplePlayer1.hs
@@ -0,0 +1,42 @@
+module SamplePlayer1 where
+
+import qualified Data.IntMap as IM
+import Data.Maybe
+import LTG
+import Play
+
+{-
+相手の生きているスロットに対してひたすら dec するだけのプレイヤー
+-}
+
+samplePlayer1 :: Player
+samplePlayer1 = Player samplePlayer1'
+
+samplePlayer1' :: GameState -> (Action, Player)
+samplePlayer1' (p0,p1) =
+  case tryToMakeN target p0 of
+    Left action -> ( action, samplePlayer1 )
+    Right i -> ( (L, Dec, i), samplePlayer1 )
+  where
+    target = findAliveTarget p1
+
+findAliveTarget :: PlayerState -> Int
+findAliveTarget (f,v) = head [i | (i, vit) <- IM.toList v, alive vit]
+
+findValue :: Value -> PlayerState -> Maybe Int
+findValue val (f,v) = listToMaybe [i | (i, val') <- IM.toList f, val==val', alive (v IM.! i)]
+
+tryToMakeN :: Int -> PlayerState -> Either Action SlotNum
+tryToMakeN n p0 =
+  case findValue (IntVal n) p0 of
+    Just i -> Right i
+    Nothing ->
+      if n > 0
+      then
+        case tryToMakeN (n-1) p0 of
+          Left action -> Left action
+          Right j -> Left (L, Succ, j)
+      else
+        case findValue (PAp I []) p0 of
+          Just idSlot -> Left (R, Zero, idSlot)
+          Nothing -> Left (L, Put, 0)
