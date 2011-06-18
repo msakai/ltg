7bd3f493dedf89959301099fe887f2f9aeab86e8
shelarcy <shelarcy@gmail.com>
2011-06-18 20:53:43 +0900

    Added 'checkSlotNum'sAccecibleSlotState' into Misc.hs
    Ignore-this: ae38e5aedd2269ad1bbfaca81060890a
    
    darcs-hash:20110618115343-ce356-8c1380053e5623bd7b9f325033f86c8f6ce7ddbc.gz

--- src/Misc.hs
+++ src/Misc.hs
@@ -1,16 +1,27 @@
 -- 思いつきで実装してみた関数などの置き場
 module Misc where
+import Data.IntMap (IntMap)
 import qualified Data.IntMap as IM
 import LTG
 
 reverseSlot :: SlotNum -> SlotNum
 reverseSlot slot = 255 - slot
 
--- 自分のi 番目と 255-i 番目の slot を見る
--- XXX 自分の i 番目の slot と、敵の 255-i 番目の slot を見る方が便利？
+-- プレイヤーの i 番目と 255-i 番目の slot を見る
 checkSlotState :: PlayerState -> SlotNum -> ((Value, Int), (Value, Int))
 checkSlotState (fstSt,sndSt) slot
   = ((getVal    fstSt slot, getVal    sndSt slot)
     ,(getRevVal fstSt slot, getRevVal sndSt slot))
-  where getVal    state = (state IM.!)
-        getRevVal state = (state IM.!) . reverseSlot
+
+
+-- 自分の i 番目の slot と、相手の 255-i 番目の slot を見る
+-- (GameStateが(自分,相手)の順番に実装されていることに依存)
+checkSlotNum'sAccecibleSlotState :: GameState -> SlotNum -> ((Value, Int), (Value, Int))
+checkSlotNum'sAccecibleSlotState ((p0fstSt,p0sndSt), (p1fstSt,p1sndSt)) slot
+  = ((getVal    p0fstSt slot, getVal    p0sndSt slot)
+    ,(getRevVal p1fstSt slot, getRevVal p1sndSt slot))
+
+
+getVal, getRevVal :: IntMap a -> SlotNum -> a
+getVal    state = (state IM.!)
+getRevVal state = (state IM.!) . reverseSlot
