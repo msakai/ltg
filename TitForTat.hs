460630e0ac5b4cb9f5fc27e3f1d6bf7766ee2feb
Masahiro Sakai <masahiro.sakai@gmail.com>
2011-06-18 15:56:31 +0900

    add TitForTat.hs
    Ignore-this: b7f04ccb47cc965c2dea6460e499758d
    
    darcs-hash:20110618065631-fad48-c1329ccbfbc2cc170eceb89a5f0889c37b6b34d3.gz

---
+++ TitForTat.hs
@@ -0,0 +1,18 @@
+{-
+相手の手をそのまま繰り返すだけのプレイヤープログラム
+-}
+module Main where
+
+import Control.Monad
+import System.Environment (getArgs)
+import LTG
+import Play
+
+main :: IO ()
+main =   do
+  args <- getArgs
+  let isPlayer0 = (args /= ["1"])
+  when isPlayer0 $ writeAction (L, Put, 0)
+  forever $ do
+    action <- readAction
+    writeAction action
