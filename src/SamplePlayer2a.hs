c3f006b86766ba23fb256574f81fefe4a1aefa89
Masahiro Sakai <masahiro.sakai@gmail.com>
2011-06-19 14:46:35 +0900

    added SamplePlayer2a.hs
    Ignore-this: b9333251110e7e7c544e19f668a32e3c
    
    darcs-hash:20110619054635-fad48-479849c26da66bf63a50aaff2fad106fb83def50.gz

---
+++ src/SamplePlayer2a.hs
@@ -0,0 +1,57 @@
+{-
+「スロット0の値iに対して、strength + 1 回 dec i する」処理を weaponLoc に作成し、
+それを callLoc にコピーしてから呼び出すというのを繰り返す。
+
+SamplePlayer2.hs をTaskモナドで書きなおした物
+-}
+module SamplePlayer2a (samplePlayer2) where
+
+import Control.Monad.State
+import qualified Data.IntMap as IM
+import Data.Maybe
+import LTG
+import Player
+import Play
+import TaskLib
+
+import SamplePlayer1 (samplePlayer1)
+
+samplePlayer2 :: Player
+samplePlayer2 = runTask' $ do
+  mkWeapon
+  forever loop
+
+weaponLoc :: SlotNum
+weaponLoc = 1
+
+callLoc :: SlotNum
+callLoc = 2
+
+strength :: Int
+strength = 330
+
+mkWeapon :: Task ()
+mkWeapon = do
+  setCard Dec weaponLoc
+  replicateM strength $ do
+    execAction (L, S, weaponLoc)
+    execAction (R, Dec, weaponLoc)
+  execAction (L, K, weaponLoc)
+  execAction (L, S, weaponLoc)
+  execAction (R, Get, weaponLoc)
+
+loop :: Task ()
+loop = do
+  (p0,p1) <- getState
+  let target = findAliveTarget p1  
+  makeNum (255 - target) 0
+  copySlot weaponLoc callLoc
+  execAction (R, Zero, callLoc)
+
+findAliveTarget :: PlayerState -> Int
+findAliveTarget (f,v) = head [i | i <- [255,254..0], alive (v IM.! i)]
+
+testSession :: IO ()
+testSession = do
+  runStateT (play samplePlayer2 samplePlayer1) initialState
+  return ()
