98ef3d005096505b76a791feb84a70ffe1ac0814
Masahiro Sakai <masahiro.sakai@gmail.com>
2011-06-19 11:14:22 +0900

    Added Task monad
    Ignore-this: f6243567e0078a8d783267d89f846aa8
    
    darcs-hash:20110619021422-fad48-496b88ba4611e25575a8911e46d95288fa1ec5ec.gz

--- src/Player.hs
+++ src/Player.hs
@@ -1,9 +1,18 @@
 module Player
-  ( Player (..)
+  ( module LTG
+
+  , Player (..)
   , replay
+
+  , Task (..)
+  , runTask'
+  , getState
+  , execAction
   ) where
 
 import LTG
+
+-- ---------------------------------------------------------------------------
 
 {-
 Player is a (infinite state) Mealy Machine
@@ -15,3 +24,24 @@ newtype Player = Player{ trans :: GameState -> (Action, Player) }
 
 replay :: [Action] -> Player
 replay (x:xs) = Player (\_ -> (x, replay xs))
+-- replay xs = runTask' (mapM_ execAction xs)
+
+-- ---------------------------------------------------------------------------
+
+-- kind of a CPS monad
+newtype Task a = Task{ runTask :: (a -> Player) -> Player }
+
+runTask' :: Task a -> Player
+runTask' m = runTask m (error "runTask'")
+
+instance Monad Task where
+  return a = Task $ \k -> k a
+  m >>= f = Task $ \k -> runTask m $ \a -> runTask (f a) k
+
+getState :: Task GameState
+getState = Task $ \k -> Player $ \s -> trans (k s) s
+
+execAction :: Action -> Task ()
+execAction a = Task $ \k -> Player $ \s -> (a, k ())
+
+-- ---------------------------------------------------------------------------
